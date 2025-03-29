cdfc0d763b600b51af745d3721db959136bea977
dat-nino <datnekk2204@gmail.com>
2025-03-29 14:07:14 +0700

    Add files via upload

---
+++ Premium.lua
@@ -0,0 +1,37 @@
+_G.Gui = "log"
+ log = "main"
+log = "setting"
+ local = "Maraki Hub & [ Premium ]"
+ local by = "dat_nino"
+loadlib = true
+
+ execute status = "delta, code x, arceus, knrl" -- mobile 
+ execute status = "ware, synz, seliware, AWP, Argon, Velocity" -- pc
+ 
+ getinfo = 2 = 1 = 10 -- setmetatable
+getinfo = "helo player turn on scripts" -- stop
+ if on true
+ end
+end
+ 
+ipairs = 9871983661872
+ len = sethook = setlocal 
+__index work 
+
+
+ select fram log10 10 
+  __mode <> Level farm = "false" -- true 
+ 
+select = "mele", "Sword", "BloxFruits"
+
+
+ __add = mele 
+local fram = __mode "all"
+ 
+ loadlib bone farm = "false" -- true
+ loadlib katakuri farm = "false" --true
+ loadlib modaura farm = "false" -- true
+ 
+ 
+ 
+ ---
