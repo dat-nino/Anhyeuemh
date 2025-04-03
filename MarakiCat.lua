70c3b7b79825f28f8788ced11f6a33b81f8b7349
dat-nino <datnekk2204@gmail.com>
2025-04-03 22:52:35 +0700

    Add files via upload

--- MarakiCat.lua
+++ MarakiCat.lua
@@ -2609,7 +2609,7 @@ function to(v233)
                 repeat
                     wait(_G.Fast_Delay);
                     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v233;
-                    wait(0.15);
+                    wait(0.05);
                     game.Players.LocalPlayer.Character.Head:Destroy();
                     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v233;
                 until ((v233.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude < 2500) and (game.Players.LocalPlayer.Character.Humanoid.Health > 0)
@@ -2704,7 +2704,7 @@ local v48 = v16.Main:AddDropdown("DropdownSelectWeapon", {
     Multi = false,
     Default = 1
 });
-v48:SetValue("Sword");
+v48:SetValue("Melee");
 v48:OnChanged(function(v236)
     ChooseWeapon = v236;
 end);
@@ -3105,8 +3105,8 @@ if Sea3 then
     end);
     local v486 = v16.Main:AddToggle("ToggleBone", {
         Title = "Bone Farm",
-        Description = "",
-        Default = true
+        Description = "Turn off is bugs",
+        Default = false
     });
     v486:OnChanged(function(v571)
         _G.AutoBone = v571;
@@ -3116,7 +3116,7 @@ if Sea3 then
             wait();
         end
     end);
-    v17.ToggleBone:SetValue(true);
+    v17.ToggleBone:SetValue(false);
     local v487 = CFrame.new(- 9515.75, 174.8521728515625, 6079.40625);
     spawn(function()
         while wait() do
